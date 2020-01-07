2f80eda46e2acf6fd5bb610d9c505dc1b741b215
Screencap <t4canty@users.noreply.github.com>
2020-01-06 19:58:31 -0800

    Update Shuffler.java

--- src/Shuffler.java
+++ src/Shuffler.java
@@ -1,4 +1,6 @@
 package src;
+
+
 /**
  * This class provides a convenient way to test shuffling methods.
  */
@@ -13,44 +15,52 @@ public class Shuffler {
 	/**
 	 * The number of values to shuffle.
 	 */
-	private static final int VALUE_COUNT = 4;
+	private static final int VALUE_COUNT = 52;
 
 	/**
 	 * Tests shuffling methods.
 	 * @param args is not used.
 	 */
 	public static void main(String[] args) {
-		System.out.println("Results of " + SHUFFLE_COUNT +
-								 " consecutive perfect shuffles:");
-		int[] values1 = new int[VALUE_COUNT];
-		for (int i = 0; i < values1.length; i++) {
-			values1[i] = i;
-			}
-		for (int j = 1; j <= SHUFFLE_COUNT; j++) {
-			perfectShuffle(values1);
-			System.out.print("  " + j + ":");
-			for (int k = 0; k < values1.length; k++) {
-				System.out.print(" " + values1[k]);
-			}
-			System.out.println();
-		}
-		System.out.println();
-
-		System.out.println("Results of " + SHUFFLE_COUNT +
-								 " consecutive efficient selection shuffles:");
-		int[] values2 = new int[VALUE_COUNT];
-		for (int i = 0; i < values2.length; i++) {
-			values2[i] = i;
-			}
-		for (int j = 1; j <= SHUFFLE_COUNT; j++) {
-			selectionShuffle(values2);
-			System.out.print("  " + j + ":");
-			for (int k = 0; k < values2.length; k++) {
-				System.out.print(" " + values2[k]);
-			}
-			System.out.println();
-		}
-		System.out.println();
+//		System.out.println("Results of " + SHUFFLE_COUNT +
+//				" consecutive perfect shuffles:");
+//		long startTime = System.currentTimeMillis();
+//		int[] values1 = new int[VALUE_COUNT];
+//		for (int i = 0; i < values1.length; i++) {
+//			values1[i] = i;
+//		}
+//		for (int j = 1; j <= SHUFFLE_COUNT; j++) {
+//			perfectShuffle(values1);
+//			System.out.print("  " + j + ":");
+//			for (int k = 0; k < 100; k++) {
+//				System.out.print(" " + values1[k]);
+//			}
+//			System.out.println();
+//		}
+//		System.out.println();
+//		long endTime = System.currentTimeMillis();
+//		System.out.println("Elapsed time: " + (endTime - startTime));
+//		System.out.println("Results of " + SHUFFLE_COUNT +
+//				" consecutive efficient selection shuffles:");
+//		startTime = System.currentTimeMillis();
+//		int[] values2 = new int[VALUE_COUNT];
+//		for (int i = 0; i < values2.length; i++) {
+//			values2[i] = i;
+//		}
+//		for (int j = 1; j <= SHUFFLE_COUNT; j++) {
+//			selectionShuffle(values2);
+//			System.out.print("  " + j + ":");
+//			for (int k = 0; k < 100; k++) {
+//				System.out.print(" " + values2[k]);
+//			}
+//			System.out.println();
+//		}
+//		System.out.println();
+//		endTime = System.currentTimeMillis();
+//		System.out.println("Elapsed time: " + (endTime - startTime));
+//		//System.out.println(headstails());
+	int[] arr1 = {1, 1, 1, 2};
+	int[] arr2 = {2, 1, 1, 1};
 	}
 
 
@@ -61,7 +71,20 @@ public class Shuffler {
 	 * @param values is an array of integers simulating cards to be shuffled.
 	 */
 	public static void perfectShuffle(int[] values) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 3 *** */
+		int shuffled[] = new int[values.length];
+		int k=0;
+		for(int i = 0; i < shuffled.length/2; i++) {
+			shuffled[k] = values[i];
+			k += 2;
+		}
+		k=1;
+		for(int i =shuffled.length/2 ; i < shuffled.length; i++) {
+			shuffled[k] = values[i];
+			k += 2;
+		}
+		for(int i = 0; i< shuffled.length; i++) {
+			values[i] = shuffled[i];
+		}
 	}
 
 	/**
@@ -76,6 +99,58 @@ public class Shuffler {
 	 * @param values is an array of integers simulating cards to be shuffled.
 	 */
 	public static void selectionShuffle(int[] values) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 3 *** */
+		for(int k = values.length-1; k >=0; k--) {
+			int j = (int) (Math.random() * k);
+			int tmp = values[k];
+			values[k] = values[j];
+			values[j] = tmp;
+		}
 	}
+
+	public static void quickShuffle(int arr[], int start, int end) {
+		if(start < end) {
+			int partition = (int) (Math.random()*(end-start) + start);
+			int tmp = arr[start];
+			arr[start] = arr[partition];
+			arr[partition] = tmp;
+			if(partition-1 > start && partition-1 > 0) {
+				quickShuffle(arr, start, partition-1);
+			}
+			if(partition+1 < end && partition+1 < arr.length-1) {
+				quickShuffle(arr, partition+1, end);
+			}
+		}
+
+	}
+
+	//questions
+	public static String headstails() {
+		int num = (int) (Math.random()*3);
+		System.out.println(num);
+		if(num == 0) {
+			return("tails");
+		}else {
+			return "heads";
+		}
+	}
+	
+	
+	public static boolean arePremunitions(int arr1[], int arr2[]) {
+		int i = 0;
+		boolean isfound = false;
+		while(i < arr1.length) {
+			for(int k = 0; k < arr2.length; k++) {
+				if(arr1[i] == arr2[k]) {
+					i++;
+					isfound = true;
+				}
+			}
+		}
+		return true;
+	}
+	
+
+
+
+
 }
